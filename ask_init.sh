b5c3826d5bbbafd39fd40c0375513752bf3ecf5b
JK <JK@git>
2016-03-03 17:39:58 +0300

    update configs

--- ask_init.sh
+++ ask_init.sh
@@ -1,10 +1,12 @@
 sudo rm /etc/nginx/sites-available/default
 sudo rm /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/gunicorn.d/test
+sudo rm /etc/gunicorn.d/test_ask
 
 sudo ln -s /home/box/web/etc/default /etc/nginx/sites-available/default
 sudo ln -s /home/box/web/etc/nginx_ask.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn_ask.conf   /etc/gunicorn.d/test_ask
 sudo /etc/init.d/gunicorn restart
