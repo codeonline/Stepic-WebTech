2b17e433471335928d08096eac17f0db7264f5d9
JK <JK@git>
2016-03-03 16:45:32 +0300

    init update

--- ask_init.sh
+++ ask_init.sh
@@ -1,6 +1,10 @@
 sudo rm /etc/nginx/sites-available/default
+sudo rm /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/gunicorn.d/test
+
 sudo ln -s /home/box/web/etc/default /etc/nginx/sites-available/default
 sudo ln -s /home/box/web/etc/nginx_ask.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
