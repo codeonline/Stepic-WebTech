f365382d6e2d9c80fb68683f0af8a91246369904
JK <JK@git>
2016-03-03 17:54:00 +0300

    url fix

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -18,11 +18,11 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     #url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'qa.views.test', name='test'),
-    url(r'^login/$', 'qa.views.test', name='test'),
-    url(r'^signup/$', 'qa.views.test', name='test'),
-    url(r'^question/\d+$', 'qa.views.test', name='test'),
-    url(r'^ask/$', 'qa.views.test', name='test'),
-    url(r'^popular/$', 'qa.views.test', name='test'),
-    url(r'^new/$', 'qa.views.test', name='test')
+    url(r'^login/', 'qa.views.test', name='test'),
+    url(r'^signup/', 'qa.views.test', name='test'),
+    url(r'^question/\d+/', 'qa.views.test', name='test'),
+    url(r'^ask/', 'qa.views.test', name='test'),
+    url(r'^popular/', 'qa.views.test', name='test'),
+    url(r'^new/', 'qa.views.test', name='test')
 )
 
