7d9c90e4879944649bfbde212233b5d3106870f6
JK <JK@git>
2016-03-03 17:24:59 +0300

    view mistype fix

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from dhango.http import HttpResponse
+from django.http import HttpResponse
+
+
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
