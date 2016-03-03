0128c1ce2efc2b072bc98f987e60c59d39fd24ff
JK <JK@git>
2016-03-03 15:46:16 +0300

    Django first config

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -3,10 +3,26 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+# Добавьте в urls.py  маршрут для следующих URL
+# /
+# /login/
+# /signup/
+# /question/<123>/    # вместо <123> - произвольный ID
+# /ask/
+# /popular/
+# /new/
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^/', 'ask.views.test', name='test'),
+    url(r'^login/', 'ask.views.test', name='test'),
+    url(r'^signup/', 'ask.views.test', name='test'),
+    url(r'^question/\d+', 'ask.views.test', name='test'),
+    url(r'^ask/', 'ask.views.test', name='test'),
+    url(r'^popular/', 'ask.views.test', name='test'),
+    url(r'^new/', 'ask.views.test', name='test')
 )
+
