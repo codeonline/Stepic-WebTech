41bcc94d92389a6aaaea4d1c105c10b345238028
JK <JK@git>
2016-03-03 16:19:40 +0300

    nginx default config fix

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+sudo rm /etc/nginx/sites-available/default
+sudo ln -s /home/box/web/etc/default /etc/nginx/sites-available/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
