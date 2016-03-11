ab788407195abd6a1c23778a2fe113e7fe754c5c
JK <JK@git>
2016-03-11 18:27:42 +0300

    QA Model and mySQL init script

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# Question - вопрос
+# title - заголовок вопроса
+# text - полный текст вопроса
+# added_at - дата добавления вопроса
+# rating - рейтинг вопроса (число)
+# author - автор вопроса
+# likes - список пользователей, поставивших "лайк"
+class Question(models.Model):
+    title = models.charField()
+    text = models.charField()
+    added_at = models.dateField()
+    rating = models.integerField()
+    author = models.OneToOneField(User)
+    likes = models.charField()
+
+
+# Answer - ответ
+# text - текст ответа
+# added_at - дата добавления ответа
+# question - вопрос, к которому относится ответ
+# author - автор ответа
+class Answer(models.Model):
+    text = models.charField()
+    added_at = models.dateField()
+    question = models.charField()
+    author = models.OneToOneField(User)
+
+
+
+
+
---
+++ ask_init_mysql.sh
@@ -0,0 +1,2 @@
+mysql -uroot -e "CREATE DATABASE ask;"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON ask.* TO 'box'@'localhost' WITH GRANT OPTION;"
