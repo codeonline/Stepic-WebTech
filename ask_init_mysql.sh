8dceb6c1e7961cadd3c51c82cc19c716d92aca65
JK_at_vitrualbox <JK@git>
2016-03-19 18:56:34 +0300

    MYSQL fix

--- ask_init_mysql.sh
+++ ask_init_mysql.sh
@@ -1,3 +1,4 @@
+sudo sed -i 's/skip-external-locking/skip-external-locking\ninnodb_use_native_aio = 0/' /etc/mysql/my.cnf
 sudo /etc/init.d/mysql start
 sudo mysql -uroot -e "CREATE DATABASE ask;"
 sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON ask.* TO 'box'@'localhost' WITH GRANT OPTION;"
